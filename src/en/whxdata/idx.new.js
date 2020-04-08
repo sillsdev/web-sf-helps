a20cf592f34f878179d0434a52e6c6ac0daa41d7
Ira Hopkinson <ira_hopkinson@sil.org>
2020-04-08 15:02:42 +0700

    Initial commit

---
+++ src/en/whxdata/idx.new.js
@@ -0,0 +1,4 @@
+(function() {
+var index =  {"type":"index","chunkinfos":[{"type":"chunkinfo","first":"Add","last":"Open","num":"106","node":"idata1"},{"type":"chunkinfo","first":"Overview","last":"Update your name","num":"101","node":"idata2"},{"type":"chunkinfo","first":"Upload an answer","last":"Versification","num":"16","node":"idata3"}]};
+window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), index, { sync:true });
+})();
