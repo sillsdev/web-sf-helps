dfdda3eef9f6f15a7abcff3c0d2e3c3931195523
Ira Hopkinson <ira_hopkinson@sil.org>
2020-08-19 17:03:18 +0700

    Update v3b es TOC
    * updated idata and toc using /web-xforge/src/Help/UpdateHelp console app

--- src/es/whxdata/toc.new.js
+++ src/es/whxdata/toc.new.js
@@ -1,4 +1,4 @@
 (function() {
-var toc =  [{"type":"book","name":"Overview","key":"toc1"},{"type":"book","name":"Concepts","key":"toc2"},{"type":"book","name":"Tasks","key":"toc3"},{"type":"book","name":"User Interface","key":"toc8"}];
+var toc =  [{"type":"book","name":"Panorama general","key":"toc1"},{"type":"book","name":"Conceptos","key":"toc2"},{"type":"book","name":"Tareas","key":"toc3"},{"type":"book","name":"Interfaz de usuario","key":"toc8"}];
 window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), toc, { sync:true });
 })();
--- src/es/whxdata/toc3.new.js
+++ src/es/whxdata/toc3.new.js
@@ -1,4 +1,4 @@
 (function() {
-var toc =  [{"type":"item","name":"Tasks overview","url":"Tasks/Tasks_overview.htm"},{"type":"book","name":"Administrator tasks","key":"toc4"},{"type":"book","name":"Basic tasks","key":"toc5"},{"type":"book","name":"Community Checking tasks","key":"toc6"},{"type":"book","name":"Translate tasks","key":"toc7"}];
+var toc =  [{"type":"item","name":"Resumen de tareas","url":"Tasks/Tasks_overview.htm"},{"type":"book","name":"Tareas de administrador","key":"toc4"},{"type":"book","name":"Tareas básicas","key":"toc5"},{"type":"book","name":"Tareas de Comprobación por la comunidad","key":"toc6"},{"type":"book","name":"Tareas de traducción","key":"toc7"}];
 window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), toc, { sync:true });
 })();
