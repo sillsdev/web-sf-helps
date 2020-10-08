e091493c6bd50da4050037070886c35d36d34c6f
Ira Hopkinson <ira_hopkinson@sil.org>
2020-10-08 15:01:56 +0700

    Add Google search for es
    - add Google Search Console site verification HTML
    - fix missing titles in TOC
    - update es translations

--- src/es/whxdata/toc1.new.js
+++ src/es/whxdata/toc1.new.js
@@ -1,4 +1,4 @@
 (function() {
-var toc =  [{"type":"item","name":"Derechos de autor y licencia","url":"Overview/Copyright_and_license.htm"},{"type":"item","name":"Preguntas frecuentes","url":"Overview/Frequently_Asked_Questions.htm"},{"type":"item","name":"Get More Help","url":"Overview/Get_More_Help.htm"},{"type":"item","name":"Empezando","url":"Overview/Getting_Started.htm"},{"type":"item","name":"Panorama general de Scripture Forge","url":"Overview/Scripture_Forge_overview.htm"}];
+var toc =  [{"type":"item","name":"Derechos de autor y licencia","url":"Overview/Copyright_and_license.htm"},{"type":"item","name":"Preguntas frecuentes","url":"Overview/Frequently_Asked_Questions.htm"},{"type":"item","name":"Obtener más ayuda","url":"Overview/Get_More_Help.htm"},{"type":"item","name":"Empezando","url":"Overview/Getting_Started.htm"},{"type":"item","name":"Panorama general de Scripture Forge","url":"Overview/Scripture_Forge_overview.htm"}];
 window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), toc, { sync:true });
 })();
