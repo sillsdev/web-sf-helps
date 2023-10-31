c386f220dd7be3e3b28b26145a23b9547a881b24
Raymond Luong <raymond_luong@sil.org>
2023-10-31 12:19:32 -0600

    Update v4.12.0

--- src/en/whxdata/idx.new.js
+++ src/en/whxdata/idx.new.js
@@ -1,4 +1,4 @@
 (function() {
-var index =  {"type":"index","chunkinfos":[{"type":"chunkinfo","first":"Add","last":"Work with answers","num":"104","node":"idata1"}]};
+var index =  {"type":"index","chunkinfos":[{"type":"chunkinfo","first":"Add","last":"Work with answers","num":"102","node":"idata1"}]};
 window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), index, { sync:true });
 })();
